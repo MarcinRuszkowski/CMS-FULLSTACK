537c4e38afc6c4c782249077c1c8226e39abe933
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2025-06-18 09:09:49 +0200

    vite config

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://cms-fullstack-c9vn.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
